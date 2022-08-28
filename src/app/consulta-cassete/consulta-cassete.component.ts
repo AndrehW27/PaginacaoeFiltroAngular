e149d46c3ac09b2b9d661ef7b555b87399b59d60
AndrehW27 <you@example.com>
2022-08-28 01:54:56 -0300

    desenvolvimento do botao filter com .push .map e .filter

--- src/app/consulta-cassete/consulta-cassete.component.ts
+++ src/app/consulta-cassete/consulta-cassete.component.ts
@@ -9,7 +9,7 @@ export class ConsultaCasseteComponent implements OnInit {
 
   public dadosAPI = [
     { casseteNome: 'Cassete 1', tipo: 'Tipo TCX', status: 'Operante' },
-    { casseteNome: 'Cassete 2', tipo: 'Tipo APX', status: 'Desativado' },
+    { casseteNome: 'Cassete 1', tipo: 'Tipo APX', status: 'Desativado' },
     { casseteNome: 'Cassete 3', tipo: 'Tipo TCX', status: 'Operante' },
     { casseteNome: 'Cassete 4', tipo: 'Tipo XPTO', status: 'Desativado' },
     { casseteNome: 'Cassete 5', tipo: 'Tipo TCX', status: 'Operante' },
@@ -21,6 +21,33 @@ export class ConsultaCasseteComponent implements OnInit {
     { casseteNome: 'Pg3 Cassete 1', tipo: 'Tipo TCX', status: 'Operante' },
     { casseteNome: 'Pg3 Cassete 2', tipo: 'Tipo APX', status: 'Desativado' },
   ]
+
+  public somenteCassestesArray: string[] = [];
+
+  public somenteCassetesFunction() {
+    for (let i = 0; i < this.dadosAPI.length; i++) {
+      this.somenteCassestesArray.push(this.dadosAPI[i].casseteNome);
+    }
+  }
+
+  public ArrCass = this.dadosAPI.map(object => object.casseteNome);
+  // this.cassetesFiltrados = this.ArrCass.filter((item) => item === 'Cassete 1');
+
+
+
+
+
+
+
+  public cassetesFiltrados: Array<string> = [];
+
+  // public Filtrar(cassetedigitado: string) {
+  //   this.cassetesFiltrados = this.ArrCass.filter((item) => item === cassetedigitado);
+  //   this.tabelaCassetes.casseteNome = this.cassetesFiltrados;
+  // }
+
+  public nomes: Array<string> = ['Andre', 'Bruna'];
+  public nomesIgualAndre = this.nomes.filter((item) => item === 'Andre')
 
   //VARIAVEIS PARA PAGINACAO
   public paginaAtual = 1;
@@ -40,13 +67,13 @@ export class ConsultaCasseteComponent implements OnInit {
       this.tabelaCassetes = this.dadosAPI.slice(this.posInicialSlice, this.posFinalSlice)
     } else {
       // alert('Página Inicial!');
-      (<HTMLButtonElement> document.getElementById("anterior")).disabled = true;
+      (<HTMLButtonElement>document.getElementById("anterior")).disabled = true;
       // (<HTMLButtonElement> document.getElementById("anterior")).style.backgroundColor = 'white';
-      (<HTMLButtonElement> document.getElementById("anterior")).style.cursor = 'auto';
+      (<HTMLButtonElement>document.getElementById("anterior")).style.cursor = 'auto';
     }
-    (<HTMLButtonElement> document.getElementById("proxima")).disabled = false;
+    (<HTMLButtonElement>document.getElementById("proxima")).disabled = false;
     // (<HTMLButtonElement> document.getElementById("proxima")).style.backgroundColor = 'white';
-    (<HTMLButtonElement> document.getElementById("proxima")).style.cursor = 'pointer';
+    (<HTMLButtonElement>document.getElementById("proxima")).style.cursor = 'pointer';
   }
 
   //FUNCAO PROXIMA
@@ -58,23 +85,23 @@ export class ConsultaCasseteComponent implements OnInit {
       this.tabelaCassetes = this.dadosAPI.slice(this.posInicialSlice, this.posFinalSlice)
     } else {
       // alert('Página Final!');
-      (<HTMLButtonElement> document.getElementById("proxima")).disabled = true;
+      (<HTMLButtonElement>document.getElementById("proxima")).disabled = true;
       // (<HTMLButtonElement> document.getElementById("proxima")).style.backgroundColor = 'white';
-      (<HTMLButtonElement> document.getElementById("proxima")).style.cursor = 'auto';
+      (<HTMLButtonElement>document.getElementById("proxima")).style.cursor = 'auto';
     }
-    (<HTMLButtonElement> document.getElementById("anterior")).disabled = false;
+    (<HTMLButtonElement>document.getElementById("anterior")).disabled = false;
     // (<HTMLButtonElement> document.getElementById("anterior")).style.backgroundColor = 'white';
-    (<HTMLButtonElement> document.getElementById("anterior")).style.cursor = 'pointer';
+    (<HTMLButtonElement>document.getElementById("anterior")).style.cursor = 'pointer';
   }
 
-  public checkUltimaPagina(){
+  public checkUltimaPagina() {
     if (this.paginaAtual === this.numeroPaginas) {
-      (<HTMLButtonElement> document.getElementById("proxima")).disabled = true;
+      (<HTMLButtonElement>document.getElementById("proxima")).disabled = true;
       // (<HTMLButtonElement> document.getElementById("proxima")).style.backgroundColor = 'white';
-      (<HTMLButtonElement> document.getElementById("proxima")).style.cursor = 'auto';
+      (<HTMLButtonElement>document.getElementById("proxima")).style.cursor = 'auto';
     }
   }
- 
+
 
   //DROPDOWN ITENS POR PAG
   public itensPorPg(event: any) {
@@ -131,6 +158,30 @@ export class ConsultaCasseteComponent implements OnInit {
   ngOnInit() {
     console.log("DADOS API:");
     console.log(this.dadosAPI);
+    console.log("");
+
+    console.log("Array somente cassetes:");
+    this.somenteCassetesFunction();
+    console.log(this.somenteCassestesArray);
+    console.log("");
+
+    console.log("Cassetes filtrados:");
+    console.log(this.cassetesFiltrados);
+    console.log("");
+
+    console.log("ARRAY NOMES");
+    console.log(this.nomes);
+    console.log("ARRAY = ANDRE");
+    console.log(this.nomesIgualAndre);
+    console.log("");
+
+
+    console.log("ArrCass:");
+    console.log(this.ArrCass);
+    console.log("");
+
+
+
   }
 
 }
